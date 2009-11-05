b8dc6e1086dceec1517082441e7b94bc48e021f6
Mike Tierney <mike@panpainter.com>
2009-11-04 17:24:11 -0800

    Animations work.

--- public/javascripts/jquery_puff.js
+++ public/javascripts/jquery_puff.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
-  $("#vcard a").hover(showVcardLabel, hideVcardLabel);
+  $("#vcard a").hover(
+    function() {
+      $("#vcard a span").show();
+      $("#vcard a span").animate({
+        top: "18px",
+        opacity: 1
+        }, 250 );
+    },
+    function() {
+      $("#vcard a span").animate({
+        top: "10px",
+        opacity: 0
+      }, 250 );
+      setTimeout("$('#vcard a span').hide();", 250);
+      $("#vcard a span").animate({
+        top: "24px",
+      }, 250 );
+    });
 });
-
-function showVcardLabel() {
-  $("#vcard a span").show();
-  $("#vcard a span").animate({
-    top: "18px",
-    opacity: 1
-    }, 250 );
-}
-
-function hideVcardLabel() {
-  $("#vcard a span").animate({
-    top: "10px",
-    opacity: 0
-  }, 250 );
-  setTimeout("$('#vcard a span').hide();", 250);
-  $("#vcard a span").animate({
-    top: "24px",
-  }, 250 );
-}
